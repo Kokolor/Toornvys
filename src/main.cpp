9a0b586230b797cd376045992fad84c1a5c74358
Kokolor <kokolor@zohomail.eu>
2025-03-08 02:31:20 +0100

    Some TT DE NEUIL

--- src/main.cpp
+++ src/main.cpp
@@ -4,86 +4,20 @@
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/TargetParser/Host.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Support/FileSystem.h"
 #include "../include/lexer.hpp"
 #include "../include/parser.hpp"
 #include "../include/codegen.hpp"
 
-std::string tokenKindToString(Token::Kind kind)
-{
-    switch (kind)
-    {
-    case Token::Kind::TOKEN_PLUS:
-        return "+";
-    case Token::Kind::TOKEN_MINUS:
-        return "-";
-    case Token::Kind::TOKEN_STAR:
-        return "*";
-    case Token::Kind::TOKEN_SLASH:
-        return "/";
-    default:
-        return "?";
-    }
-}
-
-void printAST(const Node *node, int indent = 0)
-{
-    std::string indentation(indent, ' ');
-
-    if (const NodeNumber *num = dynamic_cast<const NodeNumber *>(node))
-    {
-        std::cout << indentation << "Number(" << num->getValue() << ")\n";
-    }
-    else if (const NodeIdentifier *id = dynamic_cast<const NodeIdentifier *>(node))
-    {
-        std::cout << indentation << "Identifier(" << id->getName() << ")\n";
-    }
-    else if (const NodeBinaryOp *op = dynamic_cast<const NodeBinaryOp *>(node))
-    {
-        std::cout << indentation << "BinaryOp("
-                  << tokenKindToString(op->getOp()) << ")\n";
-        printAST(op->getLeft(), indent + 2);
-        printAST(op->getRight(), indent + 2);
-    }
-    else if (const NodeVarDeclaration *varDecl = dynamic_cast<const NodeVarDeclaration *>(node))
-    {
-        std::cout << indentation << "VarDeclaration(" << varDecl->getName() << ": " << varDecl->getType() << ")\n";
-        printAST(varDecl->getInitializer(), indent + 2);
-    }
-    else if (const NodeAssignment *assign = dynamic_cast<const NodeAssignment *>(node))
-    {
-        std::cout << indentation << "Assignment(" << assign->getName() << ")\n";
-        printAST(assign->getValue(), indent + 2);
-    }
-    else if (const NodeBlock *block = dynamic_cast<const NodeBlock *>(node))
-    {
-        std::cout << indentation << "Block\n";
-        for (const auto &stmt : block->getStatements())
-        {
-            printAST(stmt.get(), indent + 2);
-        }
-    }
-    else if (const NodeFuncDeclaration *funcDecl = dynamic_cast<const NodeFuncDeclaration *>(node))
-    {
-        std::cout << indentation << "FuncDeclaration(" << funcDecl->getName() << ")\n";
-        std::cout << indentation << "  ReturnType: " << funcDecl->getReturnType() << "\n";
-
-        std::cout << indentation << "  Args:\n";
-        for (const auto &arg : funcDecl->getArgs())
-        {
-            std::cout << indentation << "    " << arg.first << ": " << arg.second << "\n";
-        }
-
-        printAST(funcDecl->getBody().get(), indent + 2);
-    }
-    else
-    {
-        std::cout << indentation << "Unknown Node\n";
-    }
-}
-
 std::string readSourceFile(const std::string &filename)
 {
     std::ifstream file(filename);
+
     if (!file.is_open())
     {
         std::cerr << "Neuil while opening the file '" << filename << "'" << std::endl;
@@ -98,32 +32,49 @@ std::string readSourceFile(const std::string &filename)
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
+    if (argc < 3)
     {
-        std::cerr << "Usage: " << argv[0] << " <file>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <input-file> <output-file>" << std::endl;
         return 1;
     }
 
-    std::string sourceCode = readSourceFile(argv[1]);
+    std::string inputFilename = argv[1];
+    std::string outputFilename = argv[2];
+
+    std::string sourceCode = readSourceFile(inputFilename);
 
     Lexer lexer(sourceCode);
     std::vector<Token> tokens = lexer.tokenize();
 
-    // for (const Token &token : tokens)
-    // {
-    //     std::cout << "Token: " << token.getValue() << " of kind "
-    //               << static_cast<int>(token.getKind()) << std::endl;
-    // }
-
     Parser parser(tokens);
     std::unique_ptr<Node> ast = parser.parse();
-
-    // std::cout << "\nAST Structure:\n";
-    // printAST(ast.get());
 
     CodeGenerator codegen("main_module");
     codegen.generateRuntime();
     codegen.generate(ast.get());
+
+    llvm::InitializeAllTargetInfos();
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmParsers();
+    llvm::InitializeAllAsmPrinters();
+
+    std::string error;
+    auto targetTriple = llvm::sys::getDefaultTargetTriple();
+    auto target = llvm::TargetRegistry::lookupTarget(targetTriple, error);
+
+    if (!target)
+    {
+        std::cerr << error << std::endl;
+        return 1;
+    }
+
+    llvm::TargetOptions opt;
+    auto targetMachine = target->createTargetMachine(targetTriple, "generic", "", opt, llvm::Reloc::PIC_);
+
+    auto module = codegen.getModule();
+    module->setDataLayout(targetMachine->createDataLayout());
+    module->setTargetTriple(targetTriple);
 
     llvm::PassBuilder passBuilder;
     llvm::LoopAnalysisManager LAM;
@@ -138,10 +89,28 @@ int main(int argc, char *argv[])
     passBuilder.crossRegisterProxies(LAM, FAM, CGAM, MAM);
 
     llvm::ModulePassManager modulePM = passBuilder.buildPerModuleDefaultPipeline(llvm::OptimizationLevel::O3);
+    modulePM.run(*module, MAM);
 
-    modulePM.run(*codegen.getModule(), MAM);
+    std::error_code ec;
+    llvm::raw_fd_ostream dest(outputFilename.c_str(), ec, llvm::sys::fs::OF_None);
 
-    codegen.getModule()->print(llvm::outs(), nullptr);
+    if (ec)
+    {
+        std::cerr << "Could not open output file '" << outputFilename << "': " << ec.message() << std::endl;
+        return 1;
+    }
 
+    llvm::legacy::PassManager legacyPM;
+
+    if (targetMachine->addPassesToEmitFile(legacyPM, dest, nullptr, llvm::CodeGenFileType::ObjectFile))
+    {
+        std::cerr << "Failed to emit object file" << std::endl;
+        return 1;
+    }
+
+    legacyPM.run(*module);
+    dest.flush();
+
+    std::cout << "Successfully compiled '" << inputFilename << "' to object file '" << outputFilename << "'" << std::endl;
     return 0;
 }
